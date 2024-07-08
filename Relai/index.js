6cefc4dd12a0310d560b4c2112bc1b567abdec0d
charlesetienne-cote <4r86gr.sxwm7@simplelogin.co>
2024-07-08 16:59:14 -0400

    Fix MQTT

--- Relai/index.js
+++ Relai/index.js
@@ -5,6 +5,14 @@ const mqtt_address;
 
 const mqtt_client = mqtt.connect(mqtt_address);
 
+client.on("connect", () => {
+    client.subscribe("beaconid", (err) => {
+      if (!err) {
+        client.publish("beaconid", "Hello mqtt");
+      }
+    });
+  });
+
 const websockserver = new websock.Server({ port: ws_port });
 websockserver.on('connection', function (ws) {
     ws.on('error', console.error);
