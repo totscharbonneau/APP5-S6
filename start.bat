3fac7101ceb63c0617e7851a09340107e4f61c76
charlesetienne-cote <4r86gr.sxwm7@simplelogin.co>
2024-07-09 11:05:31 -0400

    Redirect output

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
-start /b mosquitto -v
-start /b node Relai/index.js
-start /b node Archive/index.js
-start /b node Control/index.js
+start /b mosquitto -v > out_mosquitto.txt
+start /b node Relai/index.js > out_relai.txt
+start /b node Archive/index.js > out_archive.txt
+start /b node Control/index.js > out_control.txt
 cd webui
-start /b ng serve
+start /b ng serve > out_webui.txt
