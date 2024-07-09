5895c61b1ded6fb193d2eb0b0d0fd543ae08e182
Thomas Charbonneau <thomas.charbonneau16@gmail.com>
2024-07-09 11:30:39 -0400

    csv start

---
+++ webui/src/app/csv-viewer/csv-viewer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-csv-viewer',
+  standalone: true,
+  imports: [],
+  templateUrl: './csv-viewer.component.html',
+  styleUrl: './csv-viewer.component.css'
+})
+export class CsvViewerComponent {
+
+}
